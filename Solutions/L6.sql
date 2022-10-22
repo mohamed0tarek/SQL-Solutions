7d423cc67eba9822d165ffef5acf3598cd17fef5
mohamed0tarek <92942683+mohamed0tarek@users.noreply.github.com>
2022-10-22 15:21:34 +0200

    Create L6.sql

---
+++ Solutions/L6.sql
@@ -0,0 +1,17 @@
+select customer_number from orders 
+group by customer_number
+having count(customer_number) >= all 
+(select count(customer_number) from orders group by customer_number)
+
+
+-----------------------
+other solution
+-----------------------
+
+SELECT
+    customer_number
+FROM
+    orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1
